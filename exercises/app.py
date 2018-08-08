4df08a356f3c43c6e5ce550d54b5af211d1b5958
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 12:15:37 +0300

    mcasckd

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,10 @@ from databases import *
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def home():
-    return 'START HERE: CHANGE ME'
-
+    #return render_template(
+    #'student.html', student_id = 4)
+    return render_template(
+    'hello.html')
 app.run(debug=True)
